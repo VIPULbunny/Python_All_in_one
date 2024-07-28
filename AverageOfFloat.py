e27221225de3a653c5f104b9c419e42799968a7e
VIPULbunny <157818450+VIPULbunny@users.noreply.github.com>
2024-07-28 19:31:46 +0530

    Add files via upload
    This file contain, 1. Addition of two numbers, 2. Area of square, 3.Average of numbers

---
+++ AverageOfFloat.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Average of two numbers is :", (num1+num2)/2 )
---
+++ variable.py
@@ -0,0 +1,7 @@
+name = "Vipul"
+age = 20
+percentage = 99.99
+
+print("My name is" ,name)
+print("my Age is ",age)
+print("My percentage is",percentage)
